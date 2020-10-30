60574935c7ceb8e4d2d5c895d20b9a2d6a51da7a
mansi_gyastelwala <mansi.gyastelwala@openxcellinc.com>
2020-10-30 10:52:46 +0530

    receive webhook started and static text removed

--- services/cronJobs.js
+++ services/cronJobs.js
@@ -4,5 +4,5 @@ var coinController = require('../controllers/v1/CoinsController');
 // On Every Minute
 cron.schedule('* * * * *', async (req, res, next) => {
     console.log("Started cron....");
-    // await coinController.returnWebhookdata();
+    await coinController.returnWebhookdata();
 });
